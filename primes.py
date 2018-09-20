ab0ff401c545bef6db8dbc186f15ca4c22fde8f9
jacob-anabi <anabi@chapman.edu>
2018-09-20 19:08:26 +0000

    fixed primes.py

--- primes.py
+++ primes.py
@@ -3,25 +3,27 @@
 import math
 
 def gen_eratosthenes():
+    index = 0
     count = 0
     list_primes = [2]
     next_prime = 3
+    yield list_primes[index]
     while (True):
-        yield list_primes[count]
-        for i in list_primes:
-            if (next_prime % i == 0):
-                break
-            elif (next_prime % i != 0) and not(next_prime in list_primes):
+        for i in range(2, next_prime):
+            if (next_prime % i != 0):
                 count += 1
+            else:
+                break
+            if (count == next_prime - 2):
+                index += 1
                 list_primes.append(next_prime)
+                yield list_primes[index]
+        count = 0
         next_prime += 1
-            
+
 def eratosthenes(n):
     p = gen_eratosthenes()
     primes = []
     for _ in range(n):
         primes.append(next(p))
     return primes
-
-print(eratosthenes(5))
-
